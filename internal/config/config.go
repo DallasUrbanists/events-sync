933afd416497c409bfbe21f108ba344433bc492c
Dominic Joseph <dtpjoseph@gmail.com>
2025-08-05 19:22:55 -0500

    wip

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,10 +4,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
+type Organization struct {
+	URL      string `json:"url"`
+	Importer string `json:"importer"`
+}
+
 type Config struct {
-	Organizations map[string]string `json:"organizations"`
+	Organizations map[string]Organization `json:"organizations"`
 }
 
 // DiscordConfig holds Discord OAuth configuration from environment variables
@@ -34,6 +43,59 @@ func LoadConfig() (*Config, error) {
 	err = decoder.Decode(&config)
 	if err != nil {
 		return nil, fmt.Errorf("error decoding config file: %v", err)
+	}
+
+	env_config_entries := Config{
+		Organizations: map[string]Organization{},
+	}
+
+	prefix := "CONFIG_ORGANIZATIONS_"
+	url_suffix := "_URL"
+	importer_suffix := "_IMPORTER"
+
+	envs := os.Environ()
+
+	fmt.Printf("Environment variables with prefix '%s':\n", prefix)
+
+	caser := cases.Title(language.English)
+	for _, env := range envs {
+		if strings.HasPrefix(env, prefix) {
+			parts := strings.SplitN(env, "=", 2)
+			if len(parts) == 2 {
+				original_key := parts[0]
+				key := strings.TrimPrefix(original_key, prefix)
+
+				if strings.HasSuffix(key, url_suffix) {
+					key = strings.TrimSuffix(key, url_suffix)
+				} else if strings.HasSuffix(key, importer_suffix) {
+					key = strings.TrimSuffix(key, importer_suffix)
+				} else {
+					continue
+				}
+
+				key = strings.ReplaceAll(key, "_", " ")
+				key = caser.String(key)
+
+				org, ok := env_config_entries.Organizations[key]
+				if !ok {
+					org = Organization{}
+				}
+
+				if strings.HasSuffix(original_key, url_suffix) {
+					org.URL = parts[1]
+				} else if strings.HasSuffix(original_key, importer_suffix) {
+					org.Importer = parts[1]
+				}
+
+				env_config_entries.Organizations[key] = org
+			}
+		}
+	}
+
+	for k, v := range env_config_entries.Organizations {
+		if v.Importer != "" && v.URL != "" {
+			config.Organizations[k] = v
+		}
 	}
 
 	return &config, nil
